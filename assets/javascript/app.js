b0587ea0b17fb1dc1efba42c71c303caa3f0d933
hliang <hliang@github>
2019-01-23 17:51:28 -0600

    better way to start and end game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyC5C0SfrL_4xvBtgveKhsVkC-Ed9iPSBTg",
@@ -16,26 +17,17 @@ var database = firebase.database();
 // initial values
 var currRound = -1;
 var totalRounds = 3;
-var sessionID = Math.random().toString(36).substr(2, 6);
+var sessionID = parseInt(moment().format("x")).toString(36); // use parseInt(sessionID, 36) to reverse this conversion
 var gameInstance = {
     "userid": 1,
     "session": {
         "sessionID": sessionID,
-        "currRound": -1,
         "palyer1Name": "",
         "palyer2Name": "",
         "player1Wins": 0,
         "player2Wins": 0,
         "ties": 0,
-        "round1": {
-            "player1Pick": "",
-            "player2Pick": "",
-        },
-        "round2": {
-            "player1Pick": "",
-            "player2Pick": "",
-        },
-        "round3": {
+        "currRound": {
             "player1Pick": "",
             "player2Pick": "",
         }
@@ -43,39 +35,33 @@ var gameInstance = {
 
     // decide who wins the current round
     fight: function (p1, p2) {
-        var round = "round" + gameInstance["session"]["currRound"];
-        var p1 = this["session"][round]["player1Pick"];
-        var p2 = this["session"][round]["player2Pick"];
-        var winner = 0;
-        if (this.session["currRound"] > 3) {
-            console.log("game over");
-            return winner;
-        }
+        var p1 = this["session"]["currRound"]["player1Pick"];
+        var p2 = this["session"]["currRound"]["player2Pick"];
+        var winner = 0; // winner id, 1 or 2. returns 0 if it's a tie.
         if (p1 === "" || p2 === "") {
             console.log("wait for another user's pick");
             return -1;
         } else if (p1 === p2) {
             this.session["ties"] += 1;
         } else {
-            this.session["round" + this.session["currRound"]]["player1Pick"] = p1;
-            this.session["round" + this.session["currRound"]]["player2Pick"] = p2;
+            this.session["currRound"]["player1Pick"] = p1;
+            this.session["currRound"]["player2Pick"] = p2;
             switch (p1 + p2) {
                 case "scissiorpaper":
                 case "rockscissior":
                 case "paperrock":
-                    this.session["round" + this.session["currRound"]]["player1Wins"] += 1;
+                    this.session["player1Wins"] += 1;
                     winner = 1;
                     console.log(p1 + " vs " + p2 + " -- player1 wins");
                     break;
                 case "paperscissior":
                 case "scissiorrock":
                 case "paperrock":
-                    this.session["round" + this.session["currRound"]]["player1Wins"] += 1;
+                    this.session["player2Wins"] += 1;
                     winner = 2;
                     console.log(p1 + " vs " + p2 + " -- player2 wins");
                     break;
             }
-            this.session["currRound"] += 1;
         }
         return winner;
     },
@@ -86,19 +72,17 @@ var gameInstance = {
         // else, retrieve session id and be the second player
         var self = this;
         console.log(self);
-        console.log("hehe")
         database.ref("rps/openSessionID").once('value', function (snapshot) {
             var openSessionID = snapshot.val();
             console.log("opensession=" + openSessionID);
             if (openSessionID == null || openSessionID == "" ) { // create new session
                 console.log(self);
                 database.ref("rps/openSessionID").set(self["session"]["sessionID"]);
-                self["session"]["currRound"] = 1;
                 // create a session
                 database.ref("rps/sessions/" + self["session"]["sessionID"]).update(self["session"]);
                 console.log("initialize remote");
                 console.log("ready player 1, with session:");
-                console.log(self["session"])
+                console.log(self["session"]);
                 return self["session"]["sessionID"];
             } else { // join existing session
                 // gameInstance["session"]["sessionID"] = openSessionID;
@@ -117,7 +101,16 @@ var gameInstance = {
     },
 
     endGame: function () {
+        var self = this;
+        // remove session stats
         database.ref("rps/sessions/" + this["session"]["sessionID"]).remove();
+        // remove opensessionid
+        database.ref("rps/openSessionID").once('value', function (snapshot) {
+            if (snapshot.val() == self["session"]["sessionID"]) {
+                database.ref("rps/openSessionID").set("");
+            }
+        });
+        return self["session"]["sessionID"];
     },
 }
 console.log("sessionID=" + gameInstance["session"]["sessionID"]);
@@ -148,8 +141,22 @@ console.log("sessionID=" + gameInstance["session"]["sessionID"]);
 //         console.log("ready player 2");
 //     }
 // });
-var sid = gameInstance.startGame();
-console.log("game started with sid=" + sid);
+
+$("#btn-start").on("click", function(){
+    var sid = gameInstance.startGame();
+    console.log("game started with sid=" + sid);
+    $(this).prop("disabled", true);
+    $("#btn-end").prop("disabled", false);
+    $("#playground").show();
+});
+
+$("#btn-end").on("click", function(){
+    var sid = gameInstance.endGame();
+    console.log("game ended sid=" + sid);
+    $(this).prop("disabled", true);
+    $("#btn-start").prop("disabled", false);
+    $("#playground").hide();
+});
 
 // // first retrieval of data
 // database.ref("rps/sessions/" + gameInstance["session"]["sessionID"]).once('value', function (snapshot) {
@@ -164,19 +171,20 @@ console.log("game started with sid=" + sid);
 // });
 
 
-// real time update
-// database.ref("rps/sessions" + gameInstance["session"]["sessionID"]).on("value",
-//     function (snapshot) {
-//         console.log("snapshot child currround=" + snapshot.child("currRound").val());
-//         if (snapshot.child("currRound").val() >= 1) {
-//             $("#player1Pick").text(snapshot.child("round1/player1Pick").val());
-//             $("#player2Pick").text(snapshot.child("round1/player2Pick").val());
-//         } else {
-//             console.log("no player picked. snapshot=" + snapshot.val());
-//         }
-//     }, function (errorObject) {
-//         console.log("The read failed: " + errorObject)
-//     });
+// real time update // TODO: how to get the sessionID?
+console.log(gameInstance["session"]["sessionID"] + "xxxxxxxxx");
+database.ref("rps/sessions" + gameInstance["session"]["sessionID"]).on("value",
+    function (snapshot) {
+        console.log("snapshot child currRound=" + snapshot.child("currRound").val());
+        if (snapshot.child("currRound").val() >= 1) {
+            $("#player1Pick").text(snapshot.child("round1/player1Pick").val());
+            $("#player2Pick").text(snapshot.child("round1/player2Pick").val());
+        } else {
+            console.log("no player picked. snapshot=" + snapshot.val());
+        }
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject)
+});
 
 console.log("instance=");
 console.log(gameInstance);
@@ -185,19 +193,18 @@ console.log(gameInstance);
 $(".btn-pick").on("click", function () {
     console.log("clicked instance=");
     console.log(gameInstance);
-    var round = "round" + gameInstance["session"]["currRound"];
     var playerpick = "player" + gameInstance["userid"] + "Pick";
-    if (gameInstance["session"][round][playerpick] == "") {
+    if (gameInstance["session"]["currRound"][playerpick] == "") {
         console.log("you just picked: " + $(this).data("pick"));
-        gameInstance["session"][round][playerpick] = $(this).data("pick");
+        gameInstance["session"]["currRound"][playerpick] = $(this).data("pick");
         // update remote
         var updates = {};
-        updates[round + "/" + playerpick] = gameInstance["session"][round][playerpick];
-        database.ref("rps/sessions").update(updates);
+        updates["currRound/" + playerpick] = gameInstance["session"]["currRound"][playerpick];
+        database.ref("rps/sessions/" + gameInstance["session"]["sessionID"]).update(updates);
     } else {
-        console.log("you already picked " + gameInstance["session"][round][playerpick]);
+        console.log("you already picked " + gameInstance["session"]["currRound"][playerpick]);
     }
     gameInstance.fight();
 });
 
-
+});
